35c92a2a9b6beca7ae85bb66abf5d8a29f477bdb
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2015-06-29 12:55:35 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/// <reference path="tmlib.js"/>
+
+(function (tm, undefined) {
+    var window = tm.global;
+    // localize
+    var assets = tm.asset.AssetManager.assets;
+
+    // const
+    var S_WIDTH = 320,
+        S_HEIGHT = 320;
+
+    var ASSETS = {
+        bg: 'assets/piyoshakebg.png',
+        normal: 'assets/hiyoco_nomal_full.png',
+        waru: 'assets/hiyoco_waru_full.png',
+        lady: 'assets/hiyoco_lady_full.png',
+        mecha: 'assets/hiyoco_mecha_full.png',
+        niwa: 'assets/hiyoco_niwatori_full.png',
+    };
+
+})(tm);
