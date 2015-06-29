54b1fc2e080f357fa063bdaaebb7dc0e091d5ef4
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2015-06-29 22:28:01 +0900

    shake

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 /// <reference path="tmlib.js"/>
 
 (function (tm, undefined) {
+
+    tm.asset.Script.loadStats();
+    // debug
+
     var window = tm.global;
     // localize
     var Math = window.Math;
@@ -64,13 +68,13 @@
         },
 
         FORM: {
-            NORMAL: createFormArray([0, 1, 2, 3]),
+            NORMAL: createFormArray([0, 1, 2, 3,2,1]),
             BIKKURI: createFormArray([4]),
             DOWN: createFormArray([5]),
-            FRONT: createFormArray([6, 7, 8]),
-            BACK: createFormArray([9, 10, 11]),
-            LEFT: createFormArray([12, 13, 14]),
-            RIGHT: createFormArray([15, 16, 17]),
+            FRONT: createFormArray([6, 7, 8,7]),
+            BACK: createFormArray([9, 10, 11,10]),
+            LEFT: createFormArray([12, 13, 14,13]),
+            RIGHT: createFormArray([15, 16, 17,16]),
             getHorizon: function (x) {
                 return x < 0 ? 'left' : 'right' ;
             },
@@ -144,50 +148,43 @@
         bikkuri: {
             frames: PIYO.FORM.BIKKURI,
             next: 'down',
-            frequency: 15,
+            frequency: 40,
         },
         down: {
             frames: PIYO.FORM.DOWN,
             next: 'bikkuri',
-            frequency: 45,
+            frequency: 90,
         },
 
         front: {
             frames: PIYO.FORM.FRONT,
             next: 'front',
-            frequency: 5,
+            frequency: 3,
         },
 
 
         back: {
             frames: PIYO.FORM.BACK,
             next: 'back',
-            frequency: 5,
+            frequency: 3,
         },
 
 
         left: {
             frames: PIYO.FORM.LEFT,
             next: 'left',
-            frequency: 5,
+            frequency: 3,
         },
 
 
         right: {
             frames: PIYO.FORM.RIGHT,
             next: 'right',
-            frequency: 5,
+            frequency: 3,
         },
     };
 
     PIYO.SHEETS = {};
-    ['normal', 'waru', 'lady', 'niwa', 'mecha'].forEach(function (e) {
-        PIYO.SHEETS[e] = tm.asset.SpriteSheet({
-            images: e,
-            frames: PIYO.FRAMES,
-            animations: PIYO.ANIMATION,
-        });
-    });
 
     var ASSETS = {
         bg: 'assets/piyoshakebg.png',
@@ -199,23 +196,45 @@
     };
 
 
+    var accel = tm.input.Accelerometer();
+    var sensors = {};
+    GRAVITIES.forEach(function (e) {
+        sensors[e] = tm.input.ShakeSensor(accel).setThresholdByGravity(e);
+    });
+
+
     // ひよこクラス
     var Hiyoko = tm.define('', {
         superClass: tm.display.AnimationSprite,
         piyo_name: null,
         form_type: null,
+        form_to: null,
         form_index: null,
+        __frame:0,
 
         init: function (name) {
-            this.superInit(PIYO.SHEETS[this.piyo_name = PIYO.SPRITES_NAME[name]], PIYO.SIZE, PIYO.SIZE);
+            this.superInit(PIYO.SHEETS[PIYO.SPRITES_NAME[name]], PIYO.SIZE, PIYO.SIZE);
             this.$extend(Hiyoko.getInitProp());
-            this.gotoAndplay(this.form_type = PIYO.FORM.getAnimationName(dx, dy));
+            this.gotoAndPlay(this.form_to = PIYO.FORM.getAnimationName(this.dx, this.dy));
 
             piyoCounter[this.form_index = PIYO.SPRITES_INDEX[name]]++;
+
+            this.form_type = PIYO.FORM.type[this.form_to];
+
+            // originalのonenterframeを使う
+            this.clearEventListener('enterframe');
+
+            Hiyoko.pushInstance(this);
+        },
+
+        onenterframe: function (e) {
+            ++this.__frame % this.currentAnimation.frequency || (this._updateFrame(),this.__frame=0);
         },
 
         toggle: function () {
-            this.gotoAndPlay(this.piyo_name = PIYO.FORM.toggle(this.piyo_name));
+            this.__frame = 0;
+            this.gotoAndPlay(this.form_to = PIYO.FORM.toggle(this.form_to));
+            this.form_type = PIYO.FORM.type[this.form_to];
             return this;
         },
 
@@ -243,7 +262,7 @@
         //枠外y
         isOutSideY: function () {
             var half = PIYO.HALF_SIZE;
-            return this.y < half || this.y > half + S_HEIGHT;
+            return this.y < half || this.y > S_HEIGHT -half;
         },
 
 
@@ -253,15 +272,25 @@
         //static
         getInitProp: function () {
             var rand = Math.random();
-            var awake = Math.random() < 0.1;
+            var speed = Hiyoko.SPEED * (Math.random() + 0.5);
+            var awake = Math.random() > 0.1;
             return {
                 x: Math.rand(PIYO.SIZE, S_WIDTH - PIYO.SIZE),
                 y: Math.rand(PIYO.SIZE, S_WIDTH - PIYO.SIZE),
-                dx: Hiyoko.SPEED * rand * PIYO.nop() * awake,
-                dy: Hiyoko.SPEED * (1 - rand) * PIYO.nop() * awake,
+                dx: speed * rand * PIYO.nop() * awake,
+                dy: speed* (1 - rand) * PIYO.nop() * awake,
             };
         },
-        SPEED: 5,
+        SPEED: 3,
+
+        instanceList: [],
+        pushInstance: function (ins) {
+            var len = this.instanceList.push(ins);
+            if (len > this.MAX_INSTANCE) {
+                this.instanceList.shift().remove();
+            }
+        },
+        MAX_INSTANCE:500,
         
     });
 
@@ -269,11 +298,11 @@
     var app;
     tm.main(function () {
         app = display.CanvasApp('#world');
-
-        app.resize(S_WIDTH, S_HEIGHT).fitWindow().fps = 60;
-
+        //window.app = app;
+        app.resize(S_WIDTH, S_HEIGHT).fitWindow();
+        app.enableStats();
         app.replaceScene(tm.game.LoadingScene({
-            nextScene: TitleScene,
+            nextScene: SetupScene,
             width: S_WIDTH,
             height: S_HEIGHT,
             assets:ASSETS,
@@ -295,16 +324,14 @@
         init: function (p) {
             this.superInit();
             if(!p)return;
-            var bg = p.bg;
-            var scene = p.next;
-            this.setNext(scene).setBG(bg);
+            this.setNext(p.next).setBG(p.bg);
             this.spriteLayer = CanvasElement().addChildTo(this);
             this.labelLayer = CanvasElement().addChildTo(this);
         },
 
         replaceScene: function (scene) {
             scene = scene || this.nextScene;
-            scene && app.replaceScene(scene);
+            scene && app.replaceScene(scene());
         },
 
         popScene: function (scene) {
@@ -318,23 +345,50 @@
         },
 
         setBG: function (bg) {
-            this.bg = Sprite(bg)
+            bg&&(this.bg = Sprite(bg)
                 .setOrigin(0, 0)
                 .setWidth(S_WIDTH)
                 .setHeight(S_HEIGHT)
-                .addChildTo(this);
+                .addChildTo(this));
             return this;
         },
 
     });
+
+    var SetupScene = tm.define('', {
+        superClass: SuperScene,
+
+        init: function () {
+            this.superInit({ next: TitleScene });
+
+            ['normal', 'waru', 'lady', 'niwa', 'mecha'].forEach(function (e) {
+                PIYO.SHEETS[e] = tm.asset.SpriteSheet({
+                    image: assets[e],
+                    frame: PIYO.FRAMES,
+                    animations: PIYO.ANIMATION,
+                });
+            });
+           // this.replaceScene();
+        },
+
+        update: function () { this.replaceScene();},
+    });
+
     var TitleScene = tm.define('', {
         superClass: SuperScene,
         init: function () {
             this.superInit({bg:'bg'});
-            this.debug = DebugLabel(100, 100).addChildTo(this.labelLayer);
+            this.debug = DebugLabel(100, 10).addChildTo(this.labelLayer);
+            for (var k in sensors) {
+                sensors[k].addChildTo(this);
+            }
         },
         update: function (app) {
-            app.frame % 30 === 0 && this.spriteLayer.addChild(Hiyoko([1,2,4,8,16].pickup()));
+            for (var k in sensors) {
+                if (sensors[k].isShaking()) {
+                    Hiyoko(k).addChildTo(this.spriteLayer);
+                }   
+            }
         },
     });
 
@@ -344,6 +398,8 @@
         init: function (x, y) {
             this.superInit();
             this.setup();
+            this.fillStyle = 'black';
+            this.setPosition(x | 0, y | 0);
 
         },
 
@@ -351,10 +407,13 @@
 
         setup: function () {
             var t = '';
+            var all = 0;
             for (var k in piyoCounter) {
-                t += PIYO.SPRITES_NAME[k] + ':' + piyoCounter[k] + '\n';
+                var c = piyoCounter[k];
+                all += c;
+                t += PIYO.SPRITES_NAME[k] + ':' + c + '\n';
             }
-            this.text = t;
+            this.text = t+'all:'+all;
         }
 
     });
